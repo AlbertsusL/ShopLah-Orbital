0a11d29c41803d59546cefe7b9d5f6c27bb80c00
brianyeo02 <brianyeo2002@gmail.com>
2025-05-21 17:36:36 +0800

    added search bar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Logo from "../../assets/logo.jpg";
+import { IoSearchOutline } from "react-icons/io5";
+import { FaCartShopping } from "react-icons/fa6"
 
 const Navbar = () => {
   return (<div>
     {/*Upper Navbar*/}
-    <div className ="shadow-md bg-white dark:bg-gray-900 dark:text-white">
-        <div className="bg-amber-400 py-2">
+    <div className ="shadow-md bg-white dark:bg-gray-900 dark:text-white
+    duration-200 relative z-40">
+        <div className="bg-amber-300 py-2">
             <div className="container flex 
             justify-between items-center">
                 <div>
@@ -15,16 +18,32 @@ const Navbar = () => {
                     ShopLah
                 </a>    
             </div>  
-            <div>
-                <div>
+            <div className="flex justify-between items-center gap-4">
+                <div className="relative group hidden sm:block">
                     <input type="text"
                     placeholder="search"
-                    className="w-[200px] sm:w-[300px] group-hover:w-[500px] 
-                    transition-all duration-200 rounded-full border border-gray-500 px-2 py-1
+                    className="w-[200px] sm:w-[200px] hover:w-[300px] 
+                    transition-all duration-200 rounded-full border border-gray-700 px-2 py-1
                     focus:outline-none focus:border-1
-                    focus:border-primary"/>
+                    focus:border-primary
+                    placeholder-black bg-white"/>   
+                    <IoSearchOutline 
+                    className="text-black
+                    hover:text-[#f3b15c] absolute top-2.5 -translate-y-0.5
+                    right-3"
+                    />
+                    </div>
                 </div>
-                </div>
+                <button onClick={
+                    () => alert("This feature not available yet")
+                } className="bg-gradient-to-r from-[#f3b15c] to-[#ed8888] 
+                transition-all duration-200 text-white py-1 px-4 rounded-full flex 
+                items-center gap-3 group">
+                    <span className="group-hover:block hidden transition-all duration-200">
+                        Order
+                    </span>
+                    <FaCartShopping className="text-xl text-white drop-shadow sm:cursor-pointer"/>
+                </button>
             </div>
         </div>
     </div>
